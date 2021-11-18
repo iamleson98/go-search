bf40707f567f53f27e1737da4683e5c73e77bfa3
Lê Văn Sơn - CMC Global DU9 <lvson1@cmcglobal.vn>
2021-11-18 15:40:04 +0700

    commit

--- textindexer/store/es/es.go
+++ textindexer/store/es/es.go
@@ -1,6 +1,8 @@
 package es
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -8,6 +10,7 @@ import (
 
 	"github.com/elastic/go-elasticsearch"
 	"github.com/elastic/go-elasticsearch/esapi"
+	"github.com/google/uuid"
 	"github.com/iamleson98/go-search/textindexer/index"
 )
 
@@ -22,14 +25,14 @@ var esMapping = `
 			"LinkID": 	 {"type": "keyword"},
 			"URL": 		 {"type": "keyword"},
 			"Content": 	 {"type": "text"},
-			"Title": 	 {"type": "date"},
+			"Title": 	 {"type": "text"},
 			"IndexedAt": {"type": "date"},
 			"PageRank":  {"type": "double"}
 		}
 	}
 }`
 
-type esSearches struct {
+type esSearchRes struct {
 	Hits esSearchResHits `json:"hits"`
 }
 
@@ -103,14 +106,167 @@ func NewElasticSearchIndexer(esNodes []string, syncUpdates bool) (*ElasticSearch
 	}, nil
 }
 
+func (i *ElasticSearchIndexer) Index(doc *index.Document) error {
+	if doc.LinkID == uuid.Nil {
+		return fmt.Errorf("index: %w", index.ErrMissingLinkID)
+	}
+
+	var (
+		buf   bytes.Buffer
+		esDoc = makeEsDoc(doc)
+	)
+	update := map[string]interface{}{
+		"doc":           esDoc,
+		"doc_as_upsert": true,
+	}
+	if err := json.NewEncoder(&buf).Encode(update); err != nil {
+		return fmt.Errorf("index: %w", err)
+	}
+
+	res, err := i.es.Update(indexName, esDoc.LinkID, &buf, i.refreshOpt)
+	if err != nil {
+		return fmt.Errorf("index: %w", err)
+	}
+
+	var updateRes esUpdateRes
+	if err = unmarshalResponse(res, &updateRes); err != nil {
+		return fmt.Errorf("index: %w", err)
+	}
+
+	return nil
+}
+
+func (i *ElasticSearchIndexer) FindByID(linkID uuid.UUID) (*index.Document, error) {
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"LinkID": linkID.String(),
+			},
+		},
+		"from": 0,
+		"size": 1,
+	}
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		return nil, fmt.Errorf("find by ID: %w", err)
+	}
+
+	searchRes, err := runSearch(i.es, query)
+	if err != nil {
+		return nil, fmt.Errorf("find by ID: %w", err)
+	}
+
+	if len(searchRes.Hits.HitList) != 1 {
+		return nil, fmt.Errorf("find by ID: %w", index.ErrNotFound)
+	}
+
+	return mapEsDoc(&searchRes.Hits.HitList[0].DocSource), nil
+}
+
+func (i *ElasticSearchIndexer) Search(q index.Query) (index.Iterator, error) {
+	var qtype string
+	switch q.Type {
+	case index.QueryTypePhrase:
+		qtype = "phrase"
+	default:
+		qtype = "best_fields"
+	}
+
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"function_score": map[string]interface{}{
+				"query": map[string]interface{}{
+					"multi_match": map[string]interface{}{
+						"type":   qtype,
+						"query":  q.Expression,
+						"fields": []string{"Title", "Content"},
+					},
+				},
+				"script_score": map[string]interface{}{
+					"source": "_score + doc['PageRank'].value",
+				},
+			},
+		},
+		"from": q.Offset,
+		"size": batchSize,
+	}
+
+	searchRes, err := runSearch(i.es, query)
+	if err != nil {
+		return nil, fmt.Errorf("search: %w", err)
+	}
+
+	return &esIterator{
+		es:        i.es,
+		searchReq: query,
+		rs:        searchRes,
+		cumIdx:    q.Offset,
+	}, nil
+}
+
+func (i *ElasticSearchIndexer) UpdateScore(linkID uuid.UUID, score float64) error {
+	var buf bytes.Buffer
+	update := map[string]interface{}{
+		"doc": map[string]interface{}{
+			"linkID":   linkID.String(),
+			"PageRank": score,
+		},
+		"doc_as_upsert": true,
+	}
+	if err := json.NewEncoder(&buf).Encode(update); err != nil {
+		return fmt.Errorf("update score: %w", err)
+	}
+
+	res, err := i.es.Update(indexName, linkID.String(), &buf, i.refreshOpt)
+	if err != nil {
+		return fmt.Errorf("update score: %w", err)
+	}
+
+	var updateRes esUpdateRes
+	if err = unmarshalResponse(res, &updateRes); err != nil {
+		return fmt.Errorf("update score: %w", err)
+	}
+
+	return nil
+}
+
 func ensureIndex(es *elasticsearch.Client) error {
 	mappingsReader := strings.NewReader(esMapping)
 	res, err := es.Indices.Create(indexName, es.Indices.Create.WithBody(mappingsReader))
 	if err != nil {
 		return fmt.Errorf("cannot create ES index: %w", err)
 	} else if res.IsError() {
-
+		err := unmarshalError(res)
+		if esErr, valid := err.(esError); valid && esErr.Type == "resource_already_exists_exception" {
+			return nil
+		}
+		return fmt.Errorf("cannot create ES index: %w", err)
 	}
+
+	return nil
+}
+
+func runSearch(es *elasticsearch.Client, searchQuery map[string]interface{}) (*esSearchRes, error) {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(searchQuery); err != nil {
+		return nil, fmt.Errorf("find by ID: %w", err)
+	}
+
+	res, err := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex(indexName),
+		es.Search.WithBody(&buf),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	var esRes esSearchRes
+	if err = unmarshalResponse(res, &esRes); err != nil {
+		return nil, err
+	}
+
+	return &esRes, nil
 }
 
 func unmarshalError(res *esapi.Response) error {
@@ -135,5 +291,22 @@ func unmarshalResponse(res *esapi.Response, to interface{}) error {
 }
 
 func mapEsDoc(d *esDoc) *index.Document {
-	return &index.Document{}
+	return &index.Document{
+		LinkID:    uuid.MustParse(d.LinkID),
+		URL:       d.URL,
+		Title:     d.Title,
+		Content:   d.Content,
+		IndexedAt: d.IndexedAt.UTC(),
+		PageRank:  d.PageRank,
+	}
+}
+
+func makeEsDoc(d *index.Document) esDoc {
+	return esDoc{
+		LinkID:    d.LinkID.String(),
+		URL:       d.URL,
+		Title:     d.Title,
+		Content:   d.Content,
+		IndexedAt: d.IndexedAt.UTC(),
+	}
 }
--- textindexer/store/es/iterator.go
+++ textindexer/store/es/iterator.go
@@ -2,6 +2,7 @@ package es
 
 import (
 	"github.com/elastic/go-elasticsearch"
+	"github.com/iamleson98/go-search/textindexer/index"
 )
 
 type esIterator struct {
@@ -10,4 +11,49 @@ type esIterator struct {
 
 	cumIdx uint64
 	rsIdx  int
+	rs     *esSearchRes
+
+	latchedDoc *index.Document
+	lastErr    error
+}
+
+func (it *esIterator) Close() error {
+	it.es = nil
+	it.searchReq = nil
+	it.cumIdx = it.rs.Hits.Total.Count
+
+	return nil
+}
+
+func (it *esIterator) Next() bool {
+	if it.lastErr != nil || it.rs == nil || it.cumIdx >= it.rs.Hits.Total.Count {
+		return false
+	}
+
+	// Do we need to fetch the next batch?
+	if it.rsIdx >= len(it.rs.Hits.HitList) {
+		it.searchReq["from"] = it.searchReq["from"].(uint64) + batchSize
+		if it.rs, it.lastErr = runSearch(it.es, it.searchReq); it.lastErr != nil {
+			return false
+		}
+
+		it.rsIdx = 0
+	}
+
+	it.latchedDoc = mapEsDoc(&it.rs.Hits.HitList[it.rsIdx].DocSource)
+	it.cumIdx++
+	it.rsIdx++
+	return true
+}
+
+func (it *esIterator) Error() error {
+	return it.lastErr
+}
+
+func (it *esIterator) Document() *index.Document {
+	return it.latchedDoc
+}
+
+func (it *esIterator) TotalCount() uint64 {
+	return it.rs.Hits.Total.Count
 }
